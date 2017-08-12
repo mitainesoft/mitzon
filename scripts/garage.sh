b9267cca9b9080d9c6a17920a9a48c3cffbd3baa
lmcjecy <jean-luc.cyr@ericsson.com>
2017-08-12 19:42:51 -0400

    Startup script at boot
    Signed-off-by: lmcjecy <jean-luc.cyr@ericsson.com>

--- scripts/garage.sh
+++ scripts/garage.sh
@@ -5,8 +5,7 @@
 # Short-Description: Starts mitainesoft garage
 # Description:       Starts mitainesoft garage
 ### END INIT INFO
-#Master
-
+# v1
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
 . /lib/init/vars.sh
 
